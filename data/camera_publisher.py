52fa3e49f348ee0add65db7b0e5626525f572575
Sebastian Ruiz <sebastian.ruiz@uni-goettingen.de>
2021-01-22 10:46:28 +0100

    fixed ros bug

--- data/camera_publisher.py
+++ data/camera_publisher.py
@@ -12,20 +12,20 @@ from std_msgs.msg import String
 if __name__ == '__main__':
     print("camera publisher node")
 
-    rospy.init_node('camera_publisher', anonymous=True)
+    rospy.init_node("camera_publisher", anonymous=True)
 
     img_path = "./example.png"
     image = cv2.imread(img_path)
     br = CvBridge()
-    pub = rospy.Publisher('/camera/image_color', Image, queue_size=50)
+    pub = rospy.Publisher("/camera/image_color", Image, queue_size=50)
     loop_rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
         
         if image is not None:
-            rospy.loginfo('publishing image')
+            rospy.loginfo("publishing image")
             pub.publish(br.cv2_to_imgmsg(image))
         else:
-            rospy.loginfo('image is none :(')
+            rospy.loginfo("image is none :(")
 
         loop_rate.sleep()
--- data/camera_subscriber.py
+++ data/camera_subscriber.py
@@ -13,13 +13,13 @@ br = CvBridge()
 def callback(msg):
     print("received image!")
     image = br.imgmsg_to_cv2(msg)
-    cv2.imwrite('./subscriber_image.png', image)
+    # cv2.imwrite('./subscriber_image.png', image)
     
 
 if __name__ == '__main__':
     print("camera subscriber node")
 
-    rospy.init_node('camera_subscriber', anonymous=True)
+    rospy.init_node("camera_subscriber", anonymous=True)
 
     loop_rate = rospy.Rate(1)
     rospy.Subscriber("/camera/image_color", Image, callback)
